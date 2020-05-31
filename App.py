9e7f62bb3c90a373fa15d3365081af7a35672998
Sergio Ortiz <owes423349@gmail.com>
2020-05-31 17:11:05 -0500

    2

--- App.py
+++ App.py
@@ -82,13 +82,51 @@ def update_employee(id):
         flash('Empleado actualizado')
         return redirect(url_for('Index'))
 
-@app.route('/add_leave')
-def add_leave():
-    return 'Añadir licencia'
+@app.route('/leave_index/<empleado>')
+def leave_index(empleado):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM licencias WHERE empleado = %s',(empleado))
+    data = cur.fetchall()
+    return render_template('leave_index.html', leave = data)
 
-@app.route('/add_holiday')
+@app.route('/add_leave/<empleado>', methods = ['POST'])
+def add_leave(empleado):
+    if request.method == 'POST':
+        employee = empleado
+        date = request.form['date']
+        motiv = request.form['motiv']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO licencias (empleado, fecha, motivo) VALUES (%s,%s,%s)", (employee,date,motiv))
+        mysql.connection.commit()
+        flash('Licencia agregada')
+        return redirect(url_for('Index'))
+
+
+@app.route('/holiday_index')
+def holiday_index():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM feriados')
+    data = cur.fetchall()
+    return render_template('holiday_index.html', holidays = data)
+
+@app.route('/add_holiday', methods = ['POST'])
 def add_holiday():
-    return 'Añadir feriado corporativo'
+    if request.method == 'POST':
+        name = request.form['name']
+        date = request.form['date']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO feriados (nombre, fecha) VALUES (%s,%s)", (name, date))
+        mysql.connection.commit()
+        flash('Feriado agregado')
+        return redirect(url_for('holiday_index'))
+
+@app.route('/delete_holiday/<string:id>', methods = ['POST','GET'])
+def delete_holiday(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM feriados WHERE id = {0}'.format(id))
+    mysql.connection.commit()
+    flash('Feriado removido')
+    return redirect(url_for('holiday_index'))
 
 @app.route('/make_slips')
 def make_slips():
